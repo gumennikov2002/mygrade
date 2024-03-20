12476ea6a9863f3dfc0adff6863ae2aa1d0f5244
Сергей Гуменников <s.gumennikov@friendlee.pro>
2024-03-20 10:40:13 +0400

    Add title

--- resources/js/Pages/Backend/Dashboard.vue
+++ resources/js/Pages/Backend/Dashboard.vue
@@ -1,9 +1,11 @@
 <template>
+    <Head title="Дашборд" />
     <DefaultLayout>
         <h1>Dashboard</h1>
     </DefaultLayout>
 </template>
 
 <script setup lang="ts">
+import { Head } from "@inertiajs/vue3";
 import DefaultLayout from "../../Layout/DefaultLayout.vue";
 </script>
