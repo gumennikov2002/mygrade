e9e065c2aadc8ddb4f53f4be71574077c34eeacf
Сергей Гуменников <s.gumennikov@friendlee.pro>
2024-03-21 19:50:00 +0400

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
         @vite(['resources/js/app.ts', 'resources/css/app.css'])
         @inertiaHead
     </head>
