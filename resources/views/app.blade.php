2d14edde54866d111b4e8582521961fa68e7bb02
Сергей Гуменников <s.gumennikov@friendlee.pro>
2024-03-22 10:48:30 +0400

    Use sass

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
-        @vite(['resources/js/app.ts', 'resources/css/app.css'])
+        @vite(['resources/js/app.ts', 'resources/css/app.scss'])
         @inertiaHead
     </head>
     <body>
